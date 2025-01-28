6514478fe97b847d4f225ab12ca45b074cd7dcdb
Mavlon4658 <mavlonbe4658>
2025-01-28 14:53:50 +0500

    project is ready

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,24 @@ if (ratingCards.length) {
         }
     })
 }
+
+const ratingModal = document.querySelector('.rating-modal');
+const ratingModalOpen = document.querySelectorAll('.rating-modal__open');
+const ratingModalBg = document.querySelector('.rating-modal__bg');
+const ratingModalBtn = document.querySelector('.rating-modal .btn-green');
+
+if (ratingModalOpen.length) {
+    ratingModalOpen.forEach(el => {
+        el.onclick = () => {
+            ratingModal.classList.add('active');
+        }
+    })
+
+    ratingModalBtn.onclick = () => {
+        ratingModal.classList.remove('active');
+    }
+
+    ratingModalBg.onclick = () => {
+        ratingModal.classList.remove('active');
+    }
+}
